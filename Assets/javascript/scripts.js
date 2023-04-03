0aac16efc6dd3616fa5f1c19208195362e6cdd02
Fox <johnafoxwell@gmail.com>
2023-04-03 19:03:54 -0400

    Added main javascript for getting weather and displaying weather

---
+++ Assets/javascript/scripts.js
@@ -0,0 +1,34 @@
+// Weather Api
+var displayWeather = document.getElementById("weather-side");
+var getButton = document.getElementById("search");
+
+getButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    var place = document.getElementById("your-search").value;
+    var city = place
+
+    function getWeather() {
+        var url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=b841c05ff9d8a8a4f747f93083d432a9"
+
+        fetch(url)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data)
+            var myCity = document.createElement("h3");
+            var myDesc = document.createElement("p");
+            var myTemp = document.createElement("p");
+            
+            myCity.textContent = data.name;
+            myDesc.textContent = data.weather[0].description;
+            myTemp.textContent = data.main.temp;
+
+            displayWeather.append(myCity);
+            displayWeather.append(myDesc);
+            displayWeather.append("The temperature in " + city + " is " + data.main.temp +" degress");
+        });
+    }
+    
+    getWeather();
+});
